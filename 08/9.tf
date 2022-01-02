978415770c73764a59332ad5acc2b0823e0013c8
tmknom <tmk.nom@gmail.com>
2022-01-02 11:54:47 +0900

    Upgrade aws_acm_certificate to AWS Provider Version 3
    See:
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-3-upgrade#resource-aws_acm_certificate

--- 08/9.tf
+++ 08/9.tf
@@ -1,4 +1,6 @@
 resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
-  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+  certificate_arn = aws_acm_certificate.example.arn
+  validation_record_fqdns = [
+    for record in aws_route53_record.example_certificate : record.fqdn
+  ]
 }
